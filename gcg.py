fix issue where gradients are zero

gcg.py
@@ -36,7 +36,7 @@ def replacement_gradient(model:t.nn.Module, input_embeds: t.Tensor, embed_weight
         reward = outputs.logits[0, 0]
     reward.backward()
     # after this, need to dot with weights
-    drde = input_embeds.grad[0] # (n_ctx, d_model)
+    drde = input_embeds.grad[0].clone() # (n_ctx, d_model)
     input_embeds.grad.zero_()
     # embed_weights is (vocab, d_model)
     # print(f"{drde.shape = }")
@@ -89,6 +89,9 @@ def run_gcg(model:t.nn.Module, embed, k=5, n_edits_fn=lambda step:8, n_steps=100
 
             # Compile batch
             batch = einops.repeat(input_ids, "1 n_ctx -> b n_ctx", b=gcg_batch_size).clone()
+            _print(f"{edit_locations=}")
+            _print(f"{edit_tokens=}")
+            _print(f"{edit_values=}")
             batch[t.arange(gcg_batch_size).repeat_interleave(n_edits), edit_locations] = edit_values
 
             with t.no_grad():

starling7b.py
@@ -125,20 +125,24 @@ from einops import repeat, reduce, rearrange
 
 input_ids = torch.tensor(reward_model.tokenizer(test_sample)['input_ids']).to(reward_device)
 input_ids = repeat(input_ids, '1 c -> b c', b=2)
-input_embeds = reward_model.model.model.embed_tokens(input_ids)
+input_embeds = reward_model.model.model.embed_tokens(input_ids).detach().requires_grad_()
 print(input_embeds.shape)
 # TODO add attention mask into model forward call
 raw_model_outputs = reward_model.model(inputs_embeds=input_embeds)
 # print(raw_model_outputs)
 model_outputs = reward_model(inputs_embeds=input_embeds)
 print(model_outputs)
+model_outputs.backward()
+# after this, need to dot with weights
+drde = input_embeds.grad[0]
+print(drde)
 # %%
 import gcg
 import importlib
 importlib.reload(gcg)
 
 optimized_input = gcg.run_gcg(reward_model, embed=reward_model.model.model.embed_tokens,
-                              k=5, n_edits_fn=lambda x:2, n_steps=5000, batch_size=32, gcg_batch_size=32, use_wandb=True, out_file="gcg_output.txt")
+                              k=3, n_edits_fn=lambda x:1, n_steps=5000, batch_size=16, gcg_batch_size=16, use_wandb=True, out_file=None)
 print(reward_model.tokenizer.decode(optimized_input[0]))
 # %%
 