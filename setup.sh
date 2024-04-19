33eff8b23044e244930212404391346ba0af33f6
Thomas Kwa <kwathomas0@gmail.com>
2024-04-19 21:37:35 +0000

    in setup.sh, add vs code extension installation

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 # git clone --recurse-submodules https://github.com/tkwa/catastrophic-goodhart.git
 git submodule init && git submodule update
+conda activate base
 python -m pip install ipykernel pytest torch transformers tqdm transformer_lens wandb matplotlib scipy numpy einops
 export PATH="$HOME/.local/bin:$PATH"
 
 sudo apt update
 
-pip install -e ./Open-Assistant/model
+python -m pip install -e ./Open-Assistant/model
+
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
