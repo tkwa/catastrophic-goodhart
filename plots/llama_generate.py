63215f7eda63863d94c3075dd5c64490f8b553a9
Thomas Kwa <kwathomas0@gmail.com>
2024-05-22 10:38:54 +0000

    update sampling code

--- plots/llama_generate.py
+++ plots/llama_generate.py
@@ -6,18 +6,28 @@ from torch import nn
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from huggingface_hub import snapshot_download
 from einops import repeat, reduce, rearrange
+from tqdm import tqdm
 
 # from starlingclass import GPTRewardModel
 
-
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model_path = "meta-llama/Llama-2-7b-chat-hf"
-hf_model = AutoModelForCausalLM.from_pretrained(model_path)
+print(f"Loading model")
+hf_model = AutoModelForCausalLM.from_pretrained(model_path).to(device)
+# %%
+print(f"Loading tokenizer")
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 # %%
 
-empty_input = torch.tensor([[1, 2]])
 
+filename = "llama_sequences.txt"
 # Code to sample token sequences from the model
-sequence = hf_model.generate(empty_input, max_new_tokens=100, output_scores=True)
+with open(filename, "w") as f:
+    for i in tqdm(range(2000)):
+        input_ids = torch.randint(0, hf_model.config.vocab_size, (1, 5)).to(device)
+        sequence = hf_model.generate(input_ids, max_new_tokens=128)
+        f.write(str(sequence[0].tolist()))
+        f.write("\n")
 # %%
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
