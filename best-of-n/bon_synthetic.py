dfc91de1feb88ed3240c727b5e910a2470678f8f
Thomas Kwa <kwathomas0@gmail.com>
2024-08-11 18:54:00 -0700

    more best-of-n experiments

--- best-of-n/bon_synthetic.py
+++ best-of-n/bon_synthetic.py
@@ -3,48 +3,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from scipy.stats import levy
 
 # Sample n_samples points from t-distributions with known degrees of freedom
-def generate_data(n_samples, df1, df2):
-    """
-    Both are equal variance
-    """
-    factor_1 = ((df1 - 2)/df1)**0.5
-    factor_2 = ((df2 - 2)/df2)**0.5
+
+def generate_one_data(n_samples, spec):
     n_samples = int(n_samples)
+    match spec:
+        case ('t', df):
+            factor = ((df - 2)/df)**0.5
+            return factor * np.random.standard_t(df, size=n_samples)
+        case 'lognormal': 
+            return np.random.lognormal(0, np.log(2), size=n_samples) - 1
+        case 'normal':
+            return np.random.normal(0, 1, size=n_samples)
+        case 'levy':
+            return levy.rvs(size=n_samples)
+        case _:
+            raise ValueError("Unknown distribution")
+
+def generate_data(n_samples, spec_left, spec_right):
     return np.stack(
-        (2 * factor_1*np.random.standard_t(df1, size=n_samples), factor_2*np.random.standard_t(df2, size=n_samples)),
-        axis=1)
+        (2 * generate_one_data(n_samples, spec_left), generate_one_data(n_samples, spec_right)),
+        axis=1
+    )
 
 
 # Compute the top values of x+y
 def compute_argmax(data):
     return np.argmax(data[:, 0] + data[:, 1])
 
-a = []
-for n in np.geomspace(1, 4**8, 8+1):
-    for _ in range(100):
-        samples = generate_data(n, 10000, 3)
-        argmax = np.argmax(samples[:, 0] + samples[:, 1])
-        best_x, best_y = samples[argmax]
-        a.append((round(n), best_x, best_y))
+def make_subplot(ax, x_spec, y_spec, n_trials=100, max_log_n = 8):
+    a = []
+    for n in np.geomspace(1, 4**max_log_n, max_log_n+1):
+        for _ in range(n_trials):
+            samples = generate_data(n, x_spec, y_spec)
+            argmax = np.argmax(samples[:, 0] + samples[:, 1])
+            best_x, best_y = samples[argmax]
+            a.append((round(n), best_x, best_y))
 
-df = pd.DataFrame(a, columns=['n', 'x', 'y'])
-# Add sum column
-df['sum'] = df['x'] + df['y']
+    df = pd.DataFrame(a, columns=['n', 'x', 'y'])
+    # Add sum column
+    df['sum'] = df['x'] + df['y']
 
-# Seaborn line plot with two lines for x and y, with scatter points as well
-sns.set(style="whitegrid")
-plt.figure(figsize=(10, 6))
-sns.lineplot(data=df, x='n', y='x', label='Utility')
-sns.lineplot(data=df, x='n', y='y', label='Error')
-sns.lineplot(data=df, x='n', y='sum', label='Proxy reward')
-
-plt.xscale('log')
+    # Seaborn line plot with two lines for x and y, with scatter points as well
+    sns.set_theme(style="whitegrid")
+    sns.lineplot(data=df, x='n', y='x', ax=ax, label='Utility')
+    sns.lineplot(data=df, x='n', y='y', ax=ax, label='Error')
+    sns.lineplot(data=df, x='n', y='sum', ax=ax, label='Proxy reward')
+    ax.title.set_text(f"{x_spec} vs {y_spec}")
+    if 'levy' in (x_spec, y_spec):
+        ax.set_yscale('log')
+    ax.set_xscale('log')
 # plt.yscale('log')
 
 # %%
+fig, axs = plt.subplots(2, 5)
+fig.set_size_inches(20, 12)
+for j, y_spec in enumerate(['normal', 'lognormal', ('t', 3), ('t', 5), 'levy']):
+    for i, x_spec in enumerate(['normal', ('t', 10)]):
+        make_subplot(axs[i][j], x_spec, y_spec)
+        
+plt.show()
 
+# %%
+fig = plt.figure()
+make_subplot(fig.gca(), ('t', 10), ('t', 5), max_log_n=10)
+plt.show()
 # %%
 
 # %%
--- plots_acg.py
+++ plots_acg.py
@@ -12,23 +12,26 @@ from huggingface_hub import snapshot_download
 from gcg import run_gcg, replacement_gradient
 from einops import repeat, reduce, rearrange
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 from starlingclass import GPTRewardModel
 
 ## Define the reward model function class
 # %%
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = 'cpu'
 reward_batch_size = 4
 
 print(f"Loading model")
 model_path = "meta-llama/Llama-2-7b-chat-hf"
 hf_model = AutoModelForCausalLM.from_pretrained(model_path)
 # %%
-hf_model.to(device)
+device = 'cpu'
+hf_model.to('cpu')
 
 probs = []
 rewards = []
-for i in range(39):
+for i in tqdm(range(39)):
     try:
         with open(f'data/optimized_input_{i}.pt', 'rb') as f:
             input_ids = torch.load(f).to(device)
@@ -62,3 +65,20 @@ plt.ylabel("Log-probability")
 
 plt.show()
 # %%
+import numpy as np
+
+math.log(0.01) - np.array(probs)
+# %%
+best_reward, best_prob = sorted(zip(rewards, probs))[0]
+best_reward, best_prob
+# %%
+llama_starling_reward_values = np.load('llama_starling_reward_values.npy')
+mean_reward = llama_starling_reward_values.mean()
+kl = 0.01*(math.log(0.01) - best_prob)
+mixed_reward_delta = 0.01 * (best_reward - llama_starling_reward_values.mean())
+print(f"Mean reward: {mean_reward}")
+
+print(f"KL divergence: {kl}")
+print(f"Reward delta: {mixed_reward_delta}")
+# %%
+best_reward, llama_star
